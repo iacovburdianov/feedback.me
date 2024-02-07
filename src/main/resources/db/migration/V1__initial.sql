c182294c5f4bee234019b40735aa1106b14858bd
Iacov Burdianov <iacov.burdianov@gmail.com>
2024-02-07 21:04:42 +0200

    Initial commit

---
+++ src/main/resources/db/migration/V1__initial.sql
@@ -0,0 +1,9 @@
+CREATE TABLE university (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE student (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__Insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO university (name) VALUES
+    ('University A'),
+    ('University B'),
+    ('University C'),
+    ('University D');
+
+
+INSERT INTO student (name) VALUES
+    ('Student 1'),
+    ('Student 2'),
+    ('Student 3'),
+    ('Student 4');
